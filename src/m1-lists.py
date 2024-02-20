7e47a489a2a15513dc43d909d540f699059f448d
kgray001 <kgray001@student.franciscan.edu>
2024-02-20 15:08:14 -0500

    finished m1

--- src/m1-lists.py
+++ src/m1-lists.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (2 pts)
+# DONE: 1. (2 pts)
 #   
 #   In this module, we will be looking at lists. Lists are exactly that, a list of items.
 #
@@ -11,25 +11,30 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+ingredients = ["chocolate chips", "cinnamon", "milk", "flour", "sugar"]
+print(ingredients)
 
 ###############################################################################
-# TODO: 2. (2 pts)
+# DONE: 2. (2 pts)
 #   
 #   For this _TODO_, write a line of code that accesses the *third* item in the list (remember the index of the first item is 0) and prints the item. Make sure you do NOT create a new list, but actually modify the original.
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+print(ingredients[2])
 
 ###############################################################################
-# TODO: 3. (2 pts)
+# DONE: 3. (2 pts)
 #   
 #   For this _TODO_, write a line of code that changes the *second* item to a different ingredient that you don't already have in your list. Once you have done this, print the list. Make sure you do NOT create a new list, but actually modify the original.
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+ingredients[1] = "nutmeg"
+print(ingredients)
 
 ###############################################################################
-# TODO: 4. (2 pts)
+# DONE: 4. (2 pts)
 #   
 #   For this _TODO_, write a line of code that adds an ingredient to the end of the list. Once you have done this, print the list. Make sure you do NOT create a new list, but actually modify the original.
 #
@@ -37,9 +42,11 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+ingredients.append("butter")
+print(ingredients)
 
 ###############################################################################
-# TODO: 5. (2 pts)
+# DONE: 5. (2 pts)
 #   
 #   For this _TODO_, write a line of code that removes the last item from the list. Once you have done this, print the list. Make sure you do NOT create a new list, but actually modify the original.
 #
@@ -47,3 +54,5 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+ingredients.pop()
+print(ingredients)
